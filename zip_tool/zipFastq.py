d1c07b81ec6b44749e91459f6393c808b0d7b8d3
Matt Garvin <mattgarvin@Matts-MacBook-Pro.local.cdis>
2019-04-16 16:42:43 -0500

    Run zipTool in parallel, improve logging.

--- zip_tool/zipFastq.py
+++ zip_tool/zipFastq.py
@@ -1,9 +1,10 @@
 from zipfile import ZipFile
-import os, json
+from joblib import Parallel, delayed
+import os, json, time
 
 def main():
     sra_list = getSRA()
-    zip(sra_list)
+    iterZip(sra_list)
 
 def mkdir(dir):
     try:
@@ -16,23 +17,48 @@ def getSRA():
     sra_list = json.load(open('uniqueSRA.json'))
     return sra_list
 
-def zip(sra_list):
+def iterZip(sra_list):
     '''Zips pairs of fastq files.'''
+    global log
+
     mkdir('zipOut')
     mkdir('zipOut/objects')
-    log = open('zipOut/log.txt', 'w')
-    for sra in sra_list:
-        zipfile = 'zipOut/objects/{}.zip'.format(sra)
-        with ZipFile(zipfile, 'w') as z:
-            log.write('-- {} --\n'.format(sra))
+
+    log = {
+    'pass': {'count': 0, 'list': []},
+    'fail': {'count': 0, 'list': []},
+    'time': 0,
+    'sra_count': len(sra_list)
+    }
+
+    print len(sra_list)
+
+    then = time.time()
+    Parallel(n_jobs=30, prefer='threads')(delayed(zip)(sra) for sra in sra_list[:30])
+    now = time.time()
+
+    dur = now - then
+    log['time'] = dur
+
+    with open('zipOut/log.json', 'w') as f:
+        f.write(json.dump(log, indent=2))
+
+def zip(sra):
+    '''Zip the two fastq's corresponding to this sra into an archive.'''
+    zipfile = 'zipOut/objects/{}.zip'.format(sra)
+    with ZipFile(zipfile, 'w') as z:
+        try:
             for i in [1,2]:
                 f = 'fastqDump/{}_{}.fastq.gz'.format(sra, i) #SRR6152696_2.fastq.gz
-                try:
-                    z.write(f)
-                    log.write('Successfully zipped {}\n'.format(f))
-                except:
-                    log.write('Error zipping {}\n'.format(f))
-    log.close()
+                z.write(f)
+            log['pass']['list'].append(sra)
+            log['pass']['count'] += 1
+        except:
+            log['fail']['list'].append(sra)
+            log['fail']['count'] += 1
+            os.remove(zipfile) # delete the faulty/empty archive
+
+
 
 if __name__ == '__main__':
     main()
