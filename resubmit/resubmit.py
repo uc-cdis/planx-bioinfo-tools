9778472582710f80d26681f1a3288a4ccb218707
Matt Garvin <mattgarvin@Matts-MacBook-Pro.local.cdis>
2019-04-23 15:27:35 -0500

    resubmit fastqs

---
+++ resubmit/resubmit.py
@@ -0,0 +1,23 @@
+import json, subprocess
+
+def main()
+    sras = getSRAs()
+    for sra in sras:
+        uploadFastq(sra)
+
+def getSRAs():
+    out = json.load(open('emptyPairs.json'))
+    return out
+
+def uploadFastq(sra):
+    cmd = [
+    '/home/ubuntu/go/bin/gen3-client',
+    'upload',
+    '--profile=matt',
+    '--upload-path=/mnt/ariba_run/fastqDump/{}_[12].fastq.gz'.format(sra)
+    ]
+
+    subprocess.call(cmd)
+    
+if __name__ == '__main__':
+    main()
